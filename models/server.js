debe81950bfefb8eda28fba1ce59d334b85af604
margaritasing <libiamfreitesm@gmail.com>
2023-04-09 17:43:57 -0300

    agrego la ruta de auth

--- models/server.js
+++ models/server.js
@@ -9,6 +9,7 @@ class Server {
         this.app  = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';   
+        this.authPath = 'api/auth'
         
         //conectar a base de datos
         this.conectarDb()
@@ -39,6 +40,7 @@ class Server {
     }
 
     routes() {
+        this.app.use( this.authPath, require('../routes/auth'));
         this.app.use( this.usuariosPath, require('../routes/user'));
     }
 
